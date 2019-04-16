aceb235baab25d39a82592b64df48b658131cdaa
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-16 22:43:55 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,2 +1,8 @@
-class Puppy 
+class Puppy
+  attr_accessor :name, :breed, :months_old
+  def initialize(name:, breed:, months_old:)
+    @name = name:
+    @breed = breed:
+    @months_old = months_old:
+  end
 end
